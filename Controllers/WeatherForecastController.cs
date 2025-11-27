1d2f6a131d9517a2198bf12a27c73bb55fb85919
MirMayasir <105602493+MirMayasir@users.noreply.github.com>
2025-11-27 15:15:02 +0530

    test

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -8,7 +8,7 @@ namespace AuthApi.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching", "windy", "cozy"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
