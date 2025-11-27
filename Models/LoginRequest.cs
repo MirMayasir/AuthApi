29829ea4e6c839697a247bdd6e6fae98a5efb575
Mayasir <mmayasi@apac.corpdir.net>
2025-11-27 13:34:26 +0530

    CI pipline added

--- Models/LoginRequest.cs
+++ Models/LoginRequest.cs
@@ -3,6 +3,7 @@ namespace AuthApi.Models
 {
     public class LoginRequest
     {
+        [Required]
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
     }
